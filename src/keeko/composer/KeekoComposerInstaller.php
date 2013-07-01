e5cda09844e3ce356389f7004f4e05bebb7542e6
gossi <github@gos.si>
2013-07-01 11:58:10 +0200

    Keeko Composer Installer

---
+++ src/keeko/composer/KeekoComposerInstaller.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace keeko\composer;
+
+use Composer\Package\PackageInterface;
+
+class UnifiedAssetInstaller extends \Composer\Installer\LibraryInstaller {
+	
+	private $types = array (
+			'keeko-core',
+			'keeko-app',
+			'keeko-module' 
+	);
+	
+	/**
+	 *
+	 * @param PackageInterface $package        	
+	 *
+	 * @return string a path relative to the root of the composer.json that is being installed.
+	 */
+	public function getInstallPath(PackageInterface $package) {
+		
+		$type = $package->getType();
+		
+		if ($type === 'keeko-core') {
+			return 'core';
+		}
+		
+		$folderMappings = array(
+			'keeko-app' => 'apps',
+			'keeko-modules' => 'modules'
+		);
+		
+		return $folderMappings[$type];
+	}
+	
+// 	/**
+// 	 * Returns the root installation path for templates.
+// 	 *
+// 	 * @return string a path relative to the root of the composer.json that is being installed where the templates
+// 	 *         are stored.
+// 	 */
+// 	protected function getTemplateRootPath() {
+// 		return (file_exists ( $this->vendorDir . '/phpdocumentor/phpdocumentor/composer.json' )) ? $this->vendorDir . '/phpdocumentor/phpdocumentor/data/templates' : 'data/templates';
+// 	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public function supports($packageType) {
+		return (bool) in_array($packageType, $this->types);
+	}
+}
