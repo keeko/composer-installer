1f53c099cb65cb51a894ce5ba69766891500fe07
gossi <github@gos.si>
2013-07-03 15:10:42 +0200

    Symlink local dev folders

--- src/keeko/composer/KeekoComposerInstaller.php
+++ src/keeko/composer/KeekoComposerInstaller.php
@@ -3,6 +3,9 @@
 namespace keeko\composer;
 
 use Composer\Package\PackageInterface;
+use Composer\Repository\InstalledRepositoryInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOException;
 
 class KeekoComposerInstaller extends \Composer\Installer\LibraryInstaller {
 	
@@ -11,6 +14,14 @@ class KeekoComposerInstaller extends \Composer\Installer\LibraryInstaller {
 		'keeko-app',
 		'keeko-module' 
 	);
+	
+	protected $symFilesystem;
+	
+	public function __construct(IOInterface $io, Composer $composer, $type = 'library') {
+		parent::__construct($io, $composer, $type);
+		
+		$this->symFilesystem = new Filesystem();
+	}
 	
 	/**
 	 *
@@ -31,7 +42,58 @@ class KeekoComposerInstaller extends \Composer\Installer\LibraryInstaller {
 			'keeko-modules' => 'modules'
 		);
 		
-		return $folderMappings[$type];
+		return $folderMappings[$type].'/'.$package->getName();
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public function install(InstalledRepositoryInterface $repo, PackageInterface $package) {
+		$local = $this->getLocalRepositoryPath();
+		$installed = false;
+		
+		if ($local !== null) {
+			$path = $local . DIRECTORY_SEPARATOR . $package->getName();
+			
+			if (file_exists($path)) {
+				try {
+					$this->symFilesystem->symlink($path, $this->getInstallPath($package));
+					$installed = true;
+				} catch(IOException $e) {
+					$installed = false;
+				}
+			}
+		}
+		
+		if (!$installed) {
+			parent::install($repo, $package);
+		}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target) {
+		if (!$repo->hasPackage($initial)) {
+			throw new \InvalidArgumentException('Package is not installed: '.$initial);
+		}
+		
+		$path = $this->getInstallPath($initial);
+		
+		if (!is_link($path)) {
+			parent::update($repo, $initial, $target);
+		}
+	}
+	
+	private function getLocalRepositoryPath() {
+		$root = $this->composer->getPackage();
+		$extra = $root->getExtra();
+		
+		if (array_key_exists('local', $extra)) {
+			return $extra['local'];
+		}
+		
+		return null;
 	}
 	
 // 	/**
